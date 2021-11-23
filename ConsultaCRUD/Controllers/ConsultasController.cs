df081e9596ba1fca7b2517610cf0a51d661b4ede
brunojgrc <brunojgrc@gmail.com>
2021-11-23 18:33:26 -0300

    Subindo arquivos do app

---
+++ ConsultaCRUD/Controllers/ConsultasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ConsultaCRUD.Models;
+
+namespace ConsultaCRUD.Controllers
+{
+    public class ConsultasController : Controller
+    {
+        private dbConsultaCadEntities db = new dbConsultaCadEntities();
+
+        // GET: Consultas
+        public ActionResult Index()
+        {
+            var consulta = db.Consulta.Include(c => c.Exame).Include(c => c.Paciente);
+            return View(consulta.ToList());
+        }
+
+        // GET: Consultas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consulta consulta = db.Consulta.Find(id);
+            if (consulta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consulta);
+        }
+
+        // GET: Consultas/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_exame = new SelectList(db.Exame, "Id_ex", "Nome_ex");
+            ViewBag.Id_paciente = new SelectList(db.Paciente, "Id_pac", "Nome_pac");
+            return View();
+        }
+
+        // POST: Consultas/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_consulta,Id_paciente,ID_exame,Data_consulta")] Consulta consulta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Consulta.Add(consulta);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_exame = new SelectList(db.Exame, "Id_ex", "Nome_ex", consulta.ID_exame);
+            ViewBag.Id_paciente = new SelectList(db.Paciente, "Id_pac", "Nome_pac", consulta.Id_paciente);
+            return View(consulta);
+        }
+
+        // GET: Consultas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consulta consulta = db.Consulta.Find(id);
+            if (consulta == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_exame = new SelectList(db.Exame, "Id_ex", "Nome_ex", consulta.ID_exame);
+            ViewBag.Id_paciente = new SelectList(db.Paciente, "Id_pac", "Nome_pac", consulta.Id_paciente);
+            return View(consulta);
+        }
+
+        // POST: Consultas/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_consulta,Id_paciente,ID_exame,Data_consulta")] Consulta consulta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(consulta).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_exame = new SelectList(db.Exame, "Id_ex", "Nome_ex", consulta.ID_exame);
+            ViewBag.Id_paciente = new SelectList(db.Paciente, "Id_pac", "Nome_pac", consulta.Id_paciente);
+            return View(consulta);
+        }
+
+        // GET: Consultas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consulta consulta = db.Consulta.Find(id);
+            if (consulta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consulta);
+        }
+
+        // POST: Consultas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Consulta consulta = db.Consulta.Find(id);
+            db.Consulta.Remove(consulta);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ConsultaCRUD/Controllers/PacientesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ConsultaCRUD.Models;
+
+namespace ConsultaCRUD.Controllers
+{
+    public class PacientesController : Controller
+    {
+        private dbConsultaCadEntities db = new dbConsultaCadEntities();
+
+        // GET: Pacientes
+        public ActionResult Index()
+        {
+            return View(db.Paciente.ToList());
+        }
+
+        // GET: Pacientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = db.Paciente.Find(id);
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // GET: Pacientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pacientes/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_pac,Nome_pac,Cpf_pac,Data_nasc_pac,Sexo_pac,Telefone_pac,Email_pac")] Paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Paciente.Add(paciente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+   
+            return View(paciente);
+        }
+
+        // GET: Pacientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = db.Paciente.Find(id);
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // POST: Pacientes/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_pac,Nome_pac,Cpf_pac,Data_nasc_pac,Sexo_pac,Telefone_pac,Email_pac")] Paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(paciente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(paciente);
+        }
+
+        // GET: Pacientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = db.Paciente.Find(id);
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // POST: Pacientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Paciente paciente = db.Paciente.Find(id);
+            db.Paciente.Remove(paciente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        /*[AcceptVerbs("GET", "POST")]
+        public ActionResult VerifyName(string Nome_pac)
+        {
+            if (!_userService.VerifyEmail(Nome_pac))
+            {
+                return Json($"Email {Nome_pac} is already in use.");
+            }
+
+            return Json(true);
+        }*/
+    }
+}
